9caeec01d4c10cb1ba2fc59d4f46800fa330d7c9
Student <example@example.com>
2019-08-16 12:50:01 -0700

    add some js functions

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#language").submit(function(event) {
+     event.preventDefault();
+    var selectedoption =  $("input:radio[name=language]:checked").val();
+    if (selectedoption === "C#") {
+      $("#cresult").show();
+
+};
+});
+});
