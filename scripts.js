cb2a947065c4fe149f9819aeea18387cdf4307c4
Student <example@example.com>
2019-08-16 16:47:33 -0700

    won't do anything

--- scripts.js
+++ scripts.js
@@ -21,7 +21,47 @@ $(document).ready(function() {
     }else if (selectedoption === "javasc") {
           $("#javascresult").show();
     }else if (selectedoption === "python") {
+      $("#pythonresult").show()
+
+
+    };
+    });
+
+
+    $("#money").submit(function(event) {
+       event.preventDefault();
+      var selectedoption =  $("input:radio[name=money]:checked").val();
+      if (selectedoption === "C#") {
+      $("#cresult").show();
+    }else if (selectedoption === "javasc") {
+          $("#javascresult").show();
+    }else if (selectedoption === "python") {
       $("#pythonresult").show();
 
     };
     });
+
+
+      $("#characteristics").submit(function(event) {
+         event.preventDefault();
+        var selectedoption =  $("input:radio[name=characteristics]:checked").val();
+        if (selectedoption === "C#") {
+        $("#cresult").show();
+      }else if (selectedoption === "javasc") {
+            $("#javascresult").show();
+      }else if (selectedoption === "python") {
+        $("#pythonresult").show();
+    };
+    });
+
+    $("#name").submit(function(event) {
+       event.preventDefault();
+      var selectedoption =  $("input:radio[name=name]:checked").val();
+      if (selectedoption === "C#") {
+      $("#cresult").show();
+    }else if (selectedoption === "javasc") {
+          $("#javascresult").show();
+    }else if (selectedoption === "python") {
+      $("#pythonresult").show();
+  };
+  });
