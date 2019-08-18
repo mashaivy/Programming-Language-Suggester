433ce74b6188eb862a2d40c1aa48ac7170d2e9ba
Masha Ivy <mashaivy9@gmail.com>
2019-08-17 19:12:44 -0700

    fix indentation

--- scripts.js
+++ scripts.js
@@ -1,84 +1,81 @@
-$(document).ready(function() {
-  $("#language").submit(function(event) {
-     event.preventDefault();
+    $(document).ready(function() {
+    $("#language").submit(function(event) {
+    event.preventDefault();
     var selectedoption =  $("input:radio[name=language]:checked").val();
     if (selectedoption === "C#") {
     $("#cresult").show();
     $("#javascresult, #pythonresult").hide();
-  }else if (selectedoption === "javasc") {
-        $("#javascresult").show();
-        $("#pythonresult, #cresult").hide();
-  }else if (selectedoption === "python") {
+    }else if (selectedoption === "javasc") {
+    $("#javascresult").show();
+    $("#pythonresult, #cresult").hide();
+    }else if (selectedoption === "python") {
     $("#pythonresult").show();
     $("#javascresult, #cresult").hide();
 
-  };
-  });
-  });
-
+    };
+    });
+    });
 
     $("#skills").submit(function(event) {
-       event.preventDefault();
-      var selectedoption =  $("input:radio[name=skills]:checked").val();
-      if (selectedoption === "C#") {
-      $("#cresult").show();
-      $("#javascresult, #pythonresult").hide();
+    event.preventDefault();
+    var selectedoption =  $("input:radio[name=skills]:checked").val();
+    if (selectedoption === "C#") {
+    $("#cresult").show();
+    $("#javascresult, #pythonresult").hide();
     }else if (selectedoption === "javasc") {
-          $("#javascresult").show();
-          $("#pythonresult, #cresult").hide();
+    $("#javascresult").show();
+    $("#pythonresult, #cresult").hide();
     }else if (selectedoption === "python") {
-      $("#pythonresult").show()
-      $("#javascresult, #cresult").hide();
-    }else {
+    $("#pythonresult").show()
+    $("#javascresult, #cresult").hide();
 
     };
     });
+
 
 
     $("#money").submit(function(event) {
-       event.preventDefault();
-      var selectedoption =  $("input:radio[name=money]:checked").val();
-      if (selectedoption === "C#") {
-      $("#cresult").show();
-      $("#javascresult, #pythonresult").hide();
+    event.preventDefault();
+    var selectedoption =  $("input:radio[name=money]:checked").val();
+    if (selectedoption === "C#") {
+    $("#cresult").show();
+    $("#javascresult, #pythonresult").hide();
     }else if (selectedoption === "javasc") {
-          $("#javascresult").show();
-          $("#pythonresult, #cresult").hide();
+    $("#javascresult").show();
+    $("#pythonresult, #cresult").hide();
     }else if (selectedoption === "python") {
-      $("#pythonresult").show();
-      $("#javascresult, #cresult").hide();
+    $("#pythonresult").show();
+    $("#javascresult, #cresult").hide();
     };
     });
 
 
-      $("#characteristics").submit(function(event) {
-         event.preventDefault();
-        var selectedoption =  $("input:radio[name=characteristics]:checked").val();
-        if (selectedoption === "C#") {
-        $("#cresult").show();
-          $("#javascresult, #pythonresult").hide();
-      }else if (selectedoption === "javasc") {
-            $("#javascresult").show();
-            $("#pythonresult, #cresult").hide();
-      }else if (selectedoption === "python") {
-        $("#pythonresult").show();
-        $("#javascresult, #cresult").hide();
+    $("#characteristics").submit(function(event) {
+    event.preventDefault();
+    var selectedoption =  $("input:radio[name=characteristics]:checked").val();
+    if (selectedoption === "C#") {
+    $("#cresult").show();
+    $("#javascresult, #pythonresult").hide();
+    }else if (selectedoption === "javasc") {
+    $("#javascresult").show();
+    $("#pythonresult, #cresult").hide();
+    }else if (selectedoption === "python") {
+    $("#pythonresult").show();
+    $("#javascresult, #cresult").hide();
     };
     });
 
     $("#name").submit(function(event) {
-       event.preventDefault();
-      var selectedoption =  $("input:radio[name=name]:checked").val();
-      if (selectedoption === "C#") {
-      $("#cresult").show();
-        $("#javascresult, #pythonresult").hide();
+    event.preventDefault();
+    var selectedoption =  $("input:radio[name=name]:checked").val();
+    if (selectedoption === "C#") {
+    $("#cresult").show();
+    $("#javascresult, #pythonresult").hide();
     }else if (selectedoption === "javasc") {
-      $("#pythonresult, #cresult").hide();
+    $("#pythonresult, #cresult").hide();
     }else if (selectedoption === "python") {
-      $("#pythonresult").show();
-        $("#javascresult, #cresult").hide();
-    }else {
-      alert("Please answer all question");
-    }
-  };
-  });
+    $("#pythonresult").show();
+    $("#javascresult, #cresult").hide();
+
+    };
+    });
